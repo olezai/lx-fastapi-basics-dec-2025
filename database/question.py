7867ef6696f7e2a070c083b135469940e28cd305
Oleksii Zaivenko <alexey.zaivenko@gmail.com>
2025-12-14 22:51:35 +0100

    experimenting with flush

--- database/question.py
+++ database/question.py
@@ -1,7 +1,17 @@
 from tinydb import TinyDB, Query
 from tinydb.storages import JSONStorage
 from tinydb.middlewares import CachingMiddleware
+import json
 
-quest_db = TinyDB("questions.json", storage=CachingMiddleware(JSONStorage))
+class CustomJSONStorage(JSONStorage):
+    def __init__(self, path, **kwargs):
+        super().__init__(path, **kwargs)
+    
+    def write(self, data):
+        # Use default=str so datetime -> ISO-like string automatically
+        with open(self.path, 'w') as f:
+            json.dump(data, f, default=str, ensure_ascii=False, indent=2)
+
+quest_db = TinyDB("questions.json", storage=CachingMiddleware(CustomJSONStorage))
 questions_table = quest_db.table("questions")
 QuestionQuery = Query()
